fe41398da0950a86ff19749899c390d0951bb396
MattIPv4 <me@mattcowley.co.uk>
2021-03-27 01:33:12 +0000

    Test multiple pandas

--- dev/index.js
+++ dev/index.js
@@ -1,5 +1,6 @@
 const Panda = require('../src');
 
 document.addEventListener('DOMContentLoaded', () => {
+    new Panda(document.body, { shootCount: 10 });
     new Panda(document.getElementById('world'), { debugMessages: true, worldScale: 2 });
 });
